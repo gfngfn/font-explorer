70472d531c8fa37733e0fd27a96311b91e6fbb43
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-11-08 11:33:48 +0900

    initial commit

---
+++ main.ml
@@ -0,0 +1,19 @@
+
+let arg_spec_list = []
+
+let font_file_loaded_ref = ref false
+
+let handle_input s =
+  font_file_loaded_ref := true;
+  let ic = open_in s in
+  ()
+
+let () =
+  Arg.parse arg_spec_list handle_input "";
+  if not !font_file_loaded_ref then
+    begin
+      print_endline "No font file specified.";
+      exit 1;
+    end
+  else
+    ()
